59cc9676e5c123842278f950634f398d70526f7b
JohnJeong123 <frenchofkiss@gmail.com>
2020-09-10 12:10:35 +0900

    added Verify API Comment

--- app/controllers/kakaocert_controller.rb
+++ app/controllers/kakaocert_controller.rb
@@ -112,6 +112,7 @@ class KakaocertController < ApplicationController
 
   # 전자서명 요청시 반환된 접수아이디를 통해 서명을 검증합니다.
   # - 서명검증시 전자서명 데이터 전문(signedData)이 반환됩니다.
+  # - 카카오페이 서비스 운영정책에 따라 검증 API는 1회만 호출할 수 있습니다. 재시도시 오류처리됩니다.
   def verifyESign
 
     # 전자서명 요청시 반환받은 접수아이디
@@ -213,6 +214,7 @@ class KakaocertController < ApplicationController
   # 본인인증 요청시 반환된 접수아이디를 통해 본인인증 서명을 검증합니다.
   # - 서명검증시 전자서명 데이터 전문(signedData)이 반환됩니다.
   # - 본인인증 요청시 작성한 Token과 서명 검증시 반환되는 signedData의 동일여부를 확인하여 본인인증 검증을 완료합니다.
+  # - 카카오페이 서비스 운영정책에 따라 검증 API는 1회만 호출할 수 있습니다. 재시도시 오류처리됩니다.
   def verifyAuth
 
     # 본인인증 요청시 반환받은 접수아이디
@@ -319,6 +321,7 @@ class KakaocertController < ApplicationController
 
   # 자동이체 출금동의 요청시 반환된 접수아이디를 통해 서명을 검증합니다.
   # - 서명검증시 전자서명 데이터 전문(signedData)이 반환됩니다.
+  # - 카카오페이 서비스 운영정책에 따라 검증 API는 1회만 호출할 수 있습니다. 재시도시 오류처리됩니다.
   def verifyCMS
 
     # 자동이체 출금동의 요청시 반환받은 접수아이디
